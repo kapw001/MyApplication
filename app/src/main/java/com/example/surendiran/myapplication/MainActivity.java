516aaaa41d5744385a3fe10b40946ef340cbd1bf
Karthik <karthik@think42labs.com>
2018-06-07 15:06:38 +0530

    some changes

--- app/src/main/java/com/example/surendiran/myapplication/MainActivity.java
+++ app/src/main/java/com/example/surendiran/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.surendiran.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+//testing changes
 public class MainActivity extends AppCompatActivity {
 
     @Override
