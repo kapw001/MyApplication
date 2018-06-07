0fd07b3142041014ab1b7eb4cbb8b81288ba65f4
Karthik <karthik@think42labs.com>
2018-06-07 15:35:34 +0530

    some changes modified

---
+++ app/src/main/java/com/example/surendiran/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.surendiran.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+//testing changes modified
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
